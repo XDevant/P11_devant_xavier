162a33181c3daee230abb97dfe31fc14227ba1d6
XDevant <xdevant@gmail.com>
2022-07-04 14:12:44 +0200

    login ok, test suites added to requirements

--- test/unit_test.py
+++ test/unit_test.py
@@ -1,4 +1,5 @@
 from test.conftest import client
+import server
 
 
 def test_index_status_code_ok(client):
@@ -6,17 +7,22 @@ def test_index_status_code_ok(client):
     assert response.status_code == 200
 
 
-def test_login_happy(client):
-    data = {'email': 'john@simplylift.co'}
-    response = client.post('/showSummary', data=data)
-    data = response.data.decode()
-    assert response.status_code == 200
-    assert 'Welcome, john@simplylift.co' in data
+class TestLoginViews:
+    clubs = [{
+        "name": "Simply Lift",
+        "email": "john@simplylift.co",
+        "points": "13"
+    }]
 
+    def test_login_happy(self, client, mocker):
+        data = {'email': 'john@simplylift.co'}
+        mocker.patch.object(server, 'clubs', self.clubs)
+        response = client.post('/showSummary', data=data)
+        assert response.status_code == 200
+        assert f"Welcome, {data['email']}" in response.data.decode()
 
-def test_login_sad(client):
-    data = {'email': 'j@s.co'}
-    response = client.post('/showSummary', data=data)
-    data = response.data.decode()
-    assert response.status_code == 200
-    assert 'Welcome to the GUDLFT Registration Portal' in data
+    def test_login_sad(self, client, mocker):
+        data = {'email': 'j@s.co'}
+        mocker.patch.object(server, 'clubs', self.clubs)
+        response = client.post('/showSummary', data=data)
+        assert response.status_code == 302
