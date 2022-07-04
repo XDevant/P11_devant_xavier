1097d3bdb7d7e7c14f41b1f6c7d58a3669d8b34f
XDevant <xdevant@gmail.com>
2022-07-04 15:25:10 +0200

    bug found if club/competition if wrong in booking, code 500

--- test/unit_test.py
+++ test/unit_test.py
@@ -26,3 +26,38 @@ class TestLoginViews:
         mocker.patch.object(server, 'clubs', self.clubs)
         response = client.post('/showSummary', data=data)
         assert response.status_code == 302
+
+
+class TestBookingView:
+    clubs = [{
+        "name": "Simply Lift",
+        "email": "john@simplylift.co",
+        "points": "13"
+    }]
+    competitions = [{
+            "name": "Spring Festival",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "25"
+        }]
+
+    def test_login_happy(self, client, mocker):
+        mocker.patch.object(server, 'clubs', self.clubs)
+        mocker.patch.object(server, 'competitions', self.competitions)
+        response = client.get('/book/Spring Festival/Simply Lift')
+        assert response.status_code == 200
+
+    def test_login_sad_competition(self, client, mocker):
+        mocker.patch.object(server, 'clubs', self.clubs)
+        mocker.patch.object(server, 'competitions', self.competitions)
+        response = client.get('/book/Spring Festival/SimplyLift')
+        assert response.status_code == 200
+
+    def test_login_sad_club(self, client, mocker):
+        mocker.patch.object(server, 'clubs', self.clubs)
+        mocker.patch.object(server, 'competitions', self.competitions)
+        response = client.get('/book/SpringFestival/Simply Lift')
+        assert response.status_code == 200
+
+
+class TestPurchaseView:
+    pass
