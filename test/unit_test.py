ec98c096189342ed84aff73fda2389e5bf2f0d70
XDevant <xdevant@gmail.com>
2022-07-07 20:52:02 +0200

    base test ok, purchase 3/6 failed

--- test/unit_test.py
+++ test/unit_test.py
@@ -1,5 +1,5 @@
 import server
-
+import pytest
 
 class TestLandingView:
     def test_index_status_code_ok(self, client):
@@ -64,46 +64,49 @@ class TestBookingView:
 
 
 class TestPurchaseView:
-    clubs = [{
-        "name": "Simply Lift",
-        "email": "john@simplylift.co",
-        "points": "13"
-    }]
-    competitions = [{
-        "name": "Spring Festival",
-        "date": "2020-03-27 10:00:00",
-        "numberOfPlaces": "5"
-    }]
+    clubs = [{"name": "foo", "points": 0}]
+    competitions = [{"name": "bar", "numberOfPlaces": 0}]
+    order = {'competition': 'bar', 'club': 'foo', 'places': 0}
+
+    def setup(self, method):
+        if method == "test_purchase_sad_not_enough_places_or_points":
+            self.competitions[0]["numberOfPlaces"] = 7
+            self.order["places"] = 8
+            print(f"Trying to buy 8 places while only 7 points/places left")
+        else:
+            self.clubs[0]["points"] = 13
+            self.competitions[0]["numberOfPlaces"] = 20
+            print(f"Trying to buy n places with enough points/places left")
 
     def test_purchase_happy(self, client, mocker):
-        data = {'competition': 'Spring Festival',
-                'club': 'Simply Lift',
-                'places': 4}
+        self.order["places"] = 8
         mocker.patch.object(server, 'clubs', self.clubs)
         mocker.patch.object(server, 'competitions', self.competitions)
-        response = client.post('/purchasePlaces', data=data)
+        response = client.post('/purchasePlaces', data=self.order)
         assert response.status_code == 200
-        assert self.competitions[0]['numberOfPlaces'] == 1
+        assert self.competitions[0]['numberOfPlaces'] == 12
+        assert self.clubs[0]["points"] == 5
 
-    def test_purchase_sad_not_enough_places(self, client, mocker):
-        data = {'competition': 'Spring Festival',
-                'club': 'Simply Lift',
-                'places': 6}
+    @pytest.mark.parametrize("points, places", [(7, 9), (9, 7)])
+    def test_purchase_sad_not_enough(self, client, mocker, points, places):
+        self.clubs[0]["points"] = points
+        self.competitions[0]["numberOfPlaces"] = places
         mocker.patch.object(server, 'clubs', self.clubs)
         mocker.patch.object(server, 'competitions', self.competitions)
-        response = client.post('/purchasePlaces', data=data)
+        response = client.post('/purchasePlaces', data=self.order)
         assert response.status_code == 200
-        assert self.competitions[0]['numberOfPlaces'] >= 0
+        assert self.clubs[0]["points"] == points
+        assert self.competitions[0]['numberOfPlaces'] == places
 
-    def test_purchase_sad_negative_purchase(self, client, mocker):
-        data = {'competition': 'Spring Festival',
-                'club': 'Simply Lift',
-                'places': -1}
+    @pytest.mark.parametrize("amount", [-1, 13])
+    def test_purchase_sad_purchase_out_of_range(self, client, mocker, amount):
+        self.order["places"] = amount
         mocker.patch.object(server, 'clubs', self.clubs)
         mocker.patch.object(server, 'competitions', self.competitions)
-        response = client.post('/purchasePlaces', data=data)
+        response = client.post('/purchasePlaces', data=self.order)
         assert response.status_code == 200
-        assert self.competitions[0]['numberOfPlaces'] == 1
+        assert self.competitions[0]['numberOfPlaces'] == 20
+        assert self.clubs[0]["points"] == 13
 
 
 class TestRankingView:
