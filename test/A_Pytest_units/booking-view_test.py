306243e29668e8ca67f4b62a97bf39f95cbe961d
XDevant <xdevant@gmail.com>
2022-07-21 23:19:06 +0200

    booking: places available in <p>

--- test/A_Pytest_units/booking-view_test.py
+++ test/A_Pytest_units/booking-view_test.py
@@ -8,7 +8,10 @@ class TestBookingView:
         mocker.patch('gudlft.utils.find_index_by_key_value', return_value=0)
         response = client.get('/book/bar/foo')
         assert response.status_code == 200
-        assert "<h2>bar</h2>" in response.data.decode()
+        content = response.data.decode()
+        assert "<h2>bar</h2>" in content
+        assert ">Places available:" in content
+        assert ">Points available:" in content
 
     def test_booking_sad_competition(self, client, mocker):
         mocker.patch('gudlft.utils.find_index_by_key_value', mock_index_return)
