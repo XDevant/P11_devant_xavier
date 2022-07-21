83326e52b3cc3ccf92dbe028f062814019e2acca
XDevant <xdevant@gmail.com>
2022-07-21 23:43:29 +0200

    home: points available correct and displayed in <p>

--- test/A_Pytest_units/home-view_test.py
+++ test/A_Pytest_units/home-view_test.py
@@ -13,13 +13,15 @@ def db():
 class TestHomeView:
 
     def test_login_happy(self, client, mocker, db):
-        form = {'email': 'foo@foo.co'}
+        form = {'email': data["clubs"][0]["email"]}
         mocker.patch('gudlft.utils.shutdown_server')
         mocker.patch('gudlft.utils.find_index_by_key_value', return_value=0)
         response = client.post('/showSummary', data=form)
         assert response.status_code == 200
         assert gudlft.utils.shutdown_server.call_count == 0
-        assert "Welcome, foo@foo.co" in response.data.decode()
+        content = response.data.decode()
+        assert f'h2>Welcome, {data["clubs"][0]["email"]}</h2' in content
+        assert f'>Points available: {data["clubs"][0]["points"]}' in content
 
     def test_login_sad(self, client, mocker, db):
         form = {'email': 'foo@bar.co'}
